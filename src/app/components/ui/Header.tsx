8ff5bf852d6c79c645152898610a1031ec10192e
Andrei Iatan <andrei.iatan00@gmail.com>
2025-01-09 13:17:19 +0200

    add basic header

--- src/app/components/ui/Header.tsx
+++ src/app/components/ui/Header.tsx
@@ -1,3 +1,12 @@
 export default function Header() {
-  return <p className="text-4xl font-title">Andrei Iatan</p>;
+  return (
+    <div className="flex h-20 items-center justify-between px-10 text-gray-200">
+      <p className="text-4xl font-title">Andrei Iatan</p>
+      <div className="flex gap-4">
+        <p>Work</p>
+        <p>About</p>
+        <p>Contact</p>
+      </div>
+    </div>
+  );
 }
